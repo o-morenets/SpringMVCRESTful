27c36f9a8bcd770951fb7fecc29981954d4f2f51
Oleksii Morenets <oleksii.morenets@gmail.com>
2019-10-18 22:53:44 +0300

    @RestController -> @Controller

--- src/main/java/org/o7planning/springmvcrestful/controller/MainRESTController.java
+++ src/main/java/org/o7planning/springmvcrestful/controller/MainRESTController.java
@@ -1,19 +1,15 @@
 package org.o7planning.springmvcrestful.controller;
 
-import java.util.List;
-
 import org.o7planning.springmvcrestful.dao.EmployeeDAO;
 import org.o7planning.springmvcrestful.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.util.List;
+
+@Controller
 public class MainRESTController {
 
 	@Autowired
